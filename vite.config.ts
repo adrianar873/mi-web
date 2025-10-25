d65aad898b735f9cff7c619be652dc6bfb1c9384
Adrian <adrian@example.com>
2025-10-25 15:25:18 +0200

    Corregir rutas base Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/adrianar873/mi-web/',
+  base: '/mi-web/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
