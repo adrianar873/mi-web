5654dd2f2b45f4266a0b25f4528e3b77d6c9f949
Adrian <adrian@example.com>
2025-10-25 14:02:12 +0200

    Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/adrianar873/mi-web',
+  base: '/https://github.com/adrianar873/mi-web/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
